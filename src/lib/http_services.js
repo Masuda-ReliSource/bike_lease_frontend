c5aab61ff3d70db82701e2cb46d35359437f07da
Masuda-Relisource <mafrin@relisource.com>
2023-03-02 13:05:57 +0600

    login with success and error message

--- src/lib/http_services.js
+++ src/lib/http_services.js
@@ -3,7 +3,7 @@ import axios from "axios";
 export const postData = async (url, data) => {
     try {
         const response = await axios.post(`${process.env.REACT_APP_BASE_URL}/${url}`, data);
-        return response.data;
+        return response;
     } catch(err){
         return err.response;
     }
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,9 +1,17 @@
 import React from 'react';
 import { useForm } from 'react-hook-form';
 import { postData } from '../lib/http_services';
+import { toast } from 'react-toastify';
 
 function Login() {
-  const { register, handleSubmit } = useForm();
+  const { register,
+          handleSubmit,
+          formState: {errors} } = useForm({
+          defaultValues: {
+            email: '',
+            password: '',
+          },
+        });
 
   const submitForm = async (data) => {
     const params = {
@@ -11,7 +19,13 @@ function Login() {
       password: data.password,
     }
     const response = await postData('sign_in', params);
-    console.log(response);
+    if (response.status === 200){
+      console.log('success');
+      toast.success('Successfully signed in.');
+    } else {
+      console.log(response);
+      toast.error(response?.data?.message);
+    }
   }
 
   return (
@@ -19,23 +33,32 @@ function Login() {
       <h1 className='text-lg text-center'>Login</h1>
       <div className='mx-auto py-4 flex items-center justify-center border-2 border-blue-300 w-1/3'>
         <form onSubmit={handleSubmit(submitForm)}>
+          {/* Email */}
           <div className='mb-4'>
             <div className='mb-2'>
               <label>Email:</label>
             </div>
-            <input {...register("email")}
+            <input {...register("email", { required: 'This is required' })}
             type="email"
             placeholder='Your email...'
             className="p-2 border-2 rounded"/>
+            <p>
+              {errors?.password?.message}
+            </p>
           </div>
+          
+          {/* Password */}
           <div className='mb-4'>
           <div className='mb-2'>
               <label>Password:</label>
             </div>
-            <input {...register("password")}
+            <input {...register("password", { required: 'This is required.' })}
             type="password"
             placeholder='Your password...'
             className='p-2 border-2 rounded'/>
+            <p>
+              {errors?.password?.message}
+            </p>
           </div>
           <div className='mt-2 flex justify-center'>
             <input
