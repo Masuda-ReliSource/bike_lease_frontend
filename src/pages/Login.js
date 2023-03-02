e66e1ccce90d362a32a31619ffd3080fcac9bed1
Masuda-Relisource <mafrin@relisource.com>
2023-03-02 11:10:16 +0600

    login page design

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -15,30 +15,37 @@ function Login() {
   }
 
   return (
-    <>
-    <h1 className='text-lg text-center'>Login</h1>
-    <form onSubmit={handleSubmit(submitForm)}>
-      <div className='mb-4'>
-        <label>Email:</label>
-        <input {...register("email")}
-        type="email"
-        placeholder='Your email...'
-        className="mx-2 p-2 border-2 rounded"/>
+    <div className='h-96 flex flex-col justify-center'>
+      <h1 className='text-lg text-center'>Login</h1>
+      <div className='mx-auto py-4 flex items-center justify-center border-2 border-blue-300 w-1/3'>
+        <form onSubmit={handleSubmit(submitForm)}>
+          <div className='mb-4'>
+            <div className='mb-2'>
+              <label>Email:</label>
+            </div>
+            <input {...register("email")}
+            type="email"
+            placeholder='Your email...'
+            className="p-2 border-2 rounded"/>
+          </div>
+          <div className='mb-4'>
+          <div className='mb-2'>
+              <label>Password:</label>
+            </div>
+            <input {...register("password")}
+            type="password"
+            placeholder='Your password...'
+            className='p-2 border-2 rounded'/>
+          </div>
+          <div className='mt-2 flex justify-center'>
+            <input
+              type='submit'
+              value='Sign In'
+              className="rounded border-2 p-2 w-full hover:bg-blue-400 hover:border-blue-400 hover:cursor-pointer"/>
+          </div>
+        </form>
       </div>
-      <div className='mb-4'>
-        <label>Password:</label>
-        <input {...register("password")}
-         type="password"
-         placeholder='Your password...'
-         className='mx-2 p-2 border-2 rounded'/>
-      </div>
-      <div>
-        <input className="rounded border-2 p-2"
-          type='submit'
-          value='Sign In'/>
-      </div>
-    </form>
-    </>
+    </div>
   )
 }
 
