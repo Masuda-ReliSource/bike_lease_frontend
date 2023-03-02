f7b669195ef3c3fb599da0723deaf06de06000f8
Masuda-Relisource <mafrin@relisource.com>
2023-03-02 17:11:13 +0600

    admin, dealer create, and admin/dealer dashboard

---
+++ src/components/home/AdminLayout.js
@@ -0,0 +1,99 @@
+import React from 'react';
+import { Breadcrumb, Layout, Menu, theme } from 'antd';
+import { useState } from 'react';
+import { RiAdminLine } from 'react-icons/ri';
+import { FcBusinessman } from 'react-icons/fc'
+
+const { Header, Content, Footer, Sider } = Layout;
+// function getItem(label, key, icon, children) {
+//   return {
+//     key,
+//     icon,
+//     children,
+//     label,
+//   };
+// }
+const items = [
+ {
+  label: 'Admin Create',
+  key: '1',
+  icon: <RiAdminLine />
+ },
+ {
+  label: 'Dealer Create',
+  key: '2',
+  icon: <FcBusinessman />
+  // children: []
+ }
+];
+
+export const AdminLayout = () => {
+  const [collapsed, setCollapsed] = useState(false);
+  const {
+    token: { colorBgContainer },
+  } = theme.useToken();
+  return (
+    <Layout
+      style={{
+        minHeight: '100vh',
+      }}
+    >
+      <Sider collapsible collapsed={collapsed} onCollapse={(value) => setCollapsed(value)}>
+        <div
+          style={{
+            height: 32,
+            margin: 16,
+            background: 'rgba(255, 255, 255, 0.2)',
+          }}
+        />
+        <Menu theme="dark"
+         defaultSelectedKeys={['1']}
+        mode="inline"
+         items={items} />
+      </Sider>
+      <Layout className="site-layout">
+        <Header
+          style={{
+            padding: 0,
+            background: colorBgContainer,
+            textAlign: 'center',
+          }}
+        >
+          Admin Portal
+          </Header>
+
+          {/* ---------------- content ------------------- */}
+
+        <Content
+          style={{
+            margin: '0 10px',
+          }}
+        >
+          <Breadcrumb
+            style={{
+              margin: '8px 0',
+            }}
+          >
+            {/* Admin Portal */}
+          </Breadcrumb>
+          <div
+            style={{
+              padding: 24,
+              minHeight: 360,
+              background: colorBgContainer,
+            }}
+          >
+            Bill is a cat.
+          </div>
+        </Content>
+        <Footer
+          style={{
+            textAlign: 'center',
+          }}
+        >
+          Bike Lease Application
+        </Footer>
+      </Layout>
+    </Layout>
+  );
+};
--- src/routes/RouteList.js
+++ src/routes/RouteList.js
@@ -3,12 +3,16 @@ import { Routes, Route } from 'react-router-dom';
 // page pages
 import Login from '../pages/Login';
 import Home from '../pages/Home';
+import AdminCreate from '../pages/AdminCreate';
+import DealerCreate from '../pages/DealerCreate';
 
 const RouteList = () => {
     return (
         <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/login" element={<Login />} />
+            <Route path="/admin/create" element={<AdminCreate />} />
+            <Route path="/dealer/create" element={<DealerCreate />} />
         </Routes>
     );
 };
