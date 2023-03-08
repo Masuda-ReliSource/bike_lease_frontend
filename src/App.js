ff29ee7b014773992d44c8f66408b3d87c9516ba
Masuda-Relisource <mafrin@relisource.com>
2023-03-08 18:21:15 +0600

    fix app page indentation

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import RouteList from './routes/RouteList';
+import RouteList from "./routes/RouteList";
 
 function App() {
   return (
-   <div className='h-screen'>
-    <h1 className='text-center'> Bike Lease Application </h1>
-    <RouteList />
-   </div>
+    <div className="h-screen">
+      <h1 className="text-center"> Bike Lease Application </h1>
+      <RouteList />
+    </div>
   );
 }
 
