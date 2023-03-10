13b01f807db987b7025e3572a04964a8ec1c4076
Masuda-Relisource <mafrin@relisource.com>
2023-03-10 16:00:21 +0600

    remove lib form and integrate with raw form

--- src/pages/AdminCreate.js
+++ src/pages/AdminCreate.js
@@ -1,23 +1,24 @@
-import React from "react";
-import { useForm } from "react-hook-form";
+import { useState } from "react";
 import { postData } from "../lib/http_services";
 import { toast } from "react-toastify";
 import { useNavigate } from "react-router-dom";
+import { FieldLabel, FormInput } from "../components/controls";
+
+const formDefaultValues = {
+  name: "",
+  email: "",
+  password: "",
+  phone: "",
+};
 
 function AdminCreate() {
+  const [data, setData] = useState(formDefaultValues);
   const navigate = useNavigate();
-  const {
-    register,
-    handleSubmit,
-    formState: { errors },
-  } = useForm({
-    defaultValues: {
-      name: "",
-      email: "",
-      password: "",
-      phone: "",
-    },
-  });
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    submitForm(data);
+  };
 
   const submitForm = async (data) => {
     const params = {
@@ -41,75 +42,64 @@ function AdminCreate() {
     }
   };
 
+  const handleChange = (e) => {
+    setData({
+      ...data,
+      [e.target.name]: e.target.value,
+    });
+  };
+
   return (
     <div className="h-3/4 flex flex-col justify-center">
       <h1 className="my-2 text-lg text-center">Create Admin</h1>
       <div className="mx-auto py-4 flex justify-center border-2 border-sky-500 w-1/3">
-        <form onSubmit={handleSubmit(submitForm)}>
+        <form onSubmit={handleSubmit}>
           {/* Name */}
           <div className="mb-4">
-            <div className="mb-2">
-              <label>
-                Name:<span className="text-red-500">*</span>
-              </label>
-            </div>
-            <input
-              {...register("name", { required: "Name is required" })}
-              type="text"
-              placeholder="eg. John Doe"
-              className="p-2 border-2 rounded hover:border-2 hover:border-sky-500 focus:outline-sky-500"
+            <FieldLabel labelName={"Name"} required={true} />
+            <FormInput
+              name={"name"}
+              type={"text"}
+              required={true}
+              placeholder={"eg. John Doe"}
+              onChangeValue={handleChange}
             />
-            <p className="text-red-500">{errors?.name?.message}</p>
           </div>
 
           {/* Email */}
           <div className="mb-4">
-            <div className="mb-2">
-              <label>
-                Email:<span className="text-red-500">*</span>
-              </label>
-            </div>
-            <input
-              {...register("email", { required: "Email is required" })}
-              type="email"
-              placeholder="eg. john@example.com"
-              className="p-2 border-2 rounded hover:border-2 hover:border-sky-500 focus:outline-sky-500"
+            <FieldLabel labelName={"Email"} required={true} />
+            <FormInput
+              name={"email"}
+              type={"email"}
+              required={true}
+              placeholder={"eg. john@example.com"}
+              onChangeValue={handleChange}
             />
-            <p className="text-red-500">{errors?.email?.message}</p>
           </div>
 
           {/* Password */}
           <div className="mb-4">
-            <div className="mb-2">
-              <label>
-                Password:<span className="text-red-500">*</span>
-              </label>
-            </div>
-            <input
-              {...register("password", { required: "Password is required" })}
-              type="password"
-              placeholder="Admin password"
-              className="p-2 border-2 rounded hover:border-2 hover:border-sky-500 
-           focus:outline-sky-500"
+            <FieldLabel labelName={"Password"} required={true} />
+            <FormInput
+              name={"password"}
+              type={"password"}
+              required={true}
+              placeholder={"Admin password"}
+              onChangeValue={handleChange}
             />
-            <p className="text-red-500">{errors?.password?.message}</p>
           </div>
 
           {/* Phone */}
           <div className="mb-4">
-            <div className="mb-2">
-              <label>
-                Phone:<span className="text-red-500">*</span>
-              </label>
-            </div>
-            <input
-              {...register("phone", { required: "Phone is required" })}
-              type="text"
-              placeholder="01928273"
-              className="p-2 border-2 rounded hover:border-2 hover:border-sky-500 
-           focus:outline-sky-500"
+            <FieldLabel labelName={"Phone"} required={true} />
+            <FormInput
+              name={"phone"}
+              type={"text"}
+              required={true}
+              placeholder={"01928273..."}
+              onChangeValue={handleChange}
             />
-            <p className="text-red-500">{errors?.password?.message}</p>
           </div>
 
           <div className="mt-2 flex justify-center">
